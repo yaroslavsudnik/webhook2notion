d8a14179a89b92c19b9f2c5c1f623ab8edee1281
Yaroslav Sudnik <yaroslav.sudnik@flatstack.com>
2020-08-18 16:23:43 +0300

    add resources endpoint

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ from flask import request
 
 app = Flask(__name__)
 
-
 token_v2 = os.environ.get("TOKEN")
 inbox_url = os.environ.get("INBOX_URL")
 tasks_url = os.environ.get("TASKS_URL")
+resources_url = os.environ.get("RESOURCES_URL")
 
 
 def addResourcesToInbox(title, source):
@@ -27,6 +27,17 @@ def addTask(name, status):
     row.status = status
 
 
+def addResource(name, source, type, status, translate):
+    client = NotionClient(token_v2)
+    cv = client.get_collection_view(resources_url)
+    row = cv.collection.add_row()
+    row.title = name
+    row.source = source
+    row.type = type
+    row.status = status
+    row.translate = translate
+
+
 @app.route('/add_inbox', methods=['POST'])
 def create_inbox():
     name = request.get_json().get('name')
@@ -40,6 +51,17 @@ def create_task():
     name = request.get_json().get('name')
     status = request.get_json().get('status')
     addTask(name, status)
+    return f'added {name} to Notion'
+
+
+@app.route('/add_resource', methods=['POST'])
+def create_resource():
+    name = request.get_json().get('name')
+    source = request.get_json().get('source')
+    type = request.get_json().get('type')
+    status = request.get_json().get('status')
+    translate = request.get_json().get('translate')
+    addResource(name, source, type, status, translate)
     return f'added {name} to Notion'
 
 
