6e3af55073fe4e015d8a6096a467877ce449be20
Yaroslav Sudnik <yaroslav.sudnik@flatstack.com>
2020-08-18 17:41:28 +0300

    add simple password checking

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import os
+import hashlib
+from setuptools._vendor.six import b
 from notion.client import NotionClient
 from flask import Flask
 from flask import request
@@ -6,9 +8,16 @@ from flask import request
 app = Flask(__name__)
 
 token_v2 = os.environ.get("TOKEN")
+password = os.environ.get("PASSWORD")
 inbox_url = os.environ.get("INBOX_URL")
 tasks_url = os.environ.get("TASKS_URL")
 resources_url = os.environ.get("RESOURCES_URL")
+
+
+def isAuthorized():
+    user_token = request.headers.environ.get('HTTP_AUTHORIZATION')
+    hash_token = hashlib.sha256(b(user_token)).hexdigest()
+    return password == hash_token
 
 
 def addResourcesToInbox(title, source):
@@ -40,29 +49,38 @@ def addResource(name, source, type, status, translate):
 
 @app.route('/add_inbox', methods=['POST'])
 def create_inbox():
-    name = request.get_json().get('name')
-    source = request.get_json().get('source')
-    addResourcesToInbox(name, source)
-    return f'added {name} to Notion'
+    if isAuthorized():
+        name = request.get_json().get('name')
+        source = request.get_json().get('source')
+        addResourcesToInbox(name, source)
+        return f'success'
+    else:
+        return f'error: unauthorized request'
 
 
 @app.route('/add_task', methods=['POST'])
 def create_task():
-    name = request.get_json().get('name')
-    status = request.get_json().get('status')
-    addTask(name, status)
-    return f'added {name} to Notion'
+    if isAuthorized():
+        name = request.get_json().get('name')
+        status = request.get_json().get('status')
+        addTask(name, status)
+        return f'success'
+    else:
+        return f'error: unauthorized request'
 
 
 @app.route('/add_resource', methods=['POST'])
 def create_resource():
-    name = request.get_json().get('name')
-    source = request.get_json().get('source')
-    type = request.get_json().get('type')
-    status = request.get_json().get('status')
-    translate = request.get_json().get('translate')
-    addResource(name, source, type, status, translate)
-    return f'added {name} to Notion'
+    if isAuthorized():
+        name = request.get_json().get('name')
+        source = request.get_json().get('source')
+        type = request.get_json().get('type')
+        status = request.get_json().get('status')
+        translate = request.get_json().get('translate')
+        addResource(name, source, type, status, translate)
+        return f'success'
+    else:
+        return f'error: unauthorized request'
 
 
 if __name__ == '__main__':
