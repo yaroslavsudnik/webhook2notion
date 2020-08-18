ee9a88d504b5c560ad0ac89621e1b96bc596ad82
Yaroslav Sudnik <yaroslav.sudnik@flatstack.com>
2020-08-18 15:01:50 +0300

    change endpoints for myself

--- app.py
+++ app.py
@@ -1,29 +1,46 @@
-
 import os
 from notion.client import NotionClient
 from flask import Flask
 from flask import request
 
-
 app = Flask(__name__)
 
 
-def createNotionTask(token, collectionURL, content):
-    # notion
-    client = NotionClient(token)
-    cv = client.get_collection_view(collectionURL)
+token_v2 = os.environ.get("TOKEN")
+inbox_url = os.environ.get("INBOX_URL")
+tasks_url = os.environ.get("TASKS_URL")
+
+
+def addResourcesToInbox(title, source):
+    client = NotionClient(token_v2)
+    cv = client.get_collection_view(inbox_url)
     row = cv.collection.add_row()
-    row.title = content
+    row.title = title
+    row.source = source
 
 
-@app.route('/create_todo', methods=['GET'])
-def create_todo():
+def addTask(name, status):
+    client = NotionClient(token_v2)
+    cv = client.get_collection_view(tasks_url)
+    row = cv.collection.add_row()
+    row.title = name
+    row.status = status
 
-    todo = request.args.get('todo')
-    token_v2 = os.environ.get("TOKEN")
-    url = os.environ.get("URL")
-    createNotionTask(token_v2, url, todo)
-    return f'added {todo} to Notion'
+
+@app.route('/add_inbox', methods=['GET'])
+def create_inbox():
+    name = request.args.get('name')
+    source = request.args.get('source')
+    addResourcesToInbox(name, source)
+    return f'added {name} to Notion'
+
+
+@app.route('/add_task', methods=['GET'])
+def create_task():
+    name = request.args.get('name')
+    status = request.args.get('status')
+    addTask(name, status)
+    return f'added {name} to Notion'
 
 
 if __name__ == '__main__':
