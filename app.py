718ad25b56cb9e399f7ae1f07240596c98148363
Yaroslav Sudnik <yaroslav.sudnik@flatstack.com>
2020-08-18 18:17:10 +0300

    add status codes for requests

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ from flask import request
 
 app = Flask(__name__)
 
+HTTP_201_CREATED = 201
+HTTP_401_UNAUTHORIZED = 401
+
 token_v2 = os.environ.get("TOKEN")
 password = os.environ.get("PASSWORD")
 inbox_url = os.environ.get("INBOX_URL")
@@ -14,13 +17,13 @@ tasks_url = os.environ.get("TASKS_URL")
 resources_url = os.environ.get("RESOURCES_URL")
 
 
-def isAuthorized():
+def is_authorized():
     user_token = request.headers.environ.get('HTTP_AUTHORIZATION')
     hash_token = hashlib.sha256(b(user_token)).hexdigest()
     return password == hash_token
 
 
-def addResourcesToInbox(title, source):
+def add_resources_to_inbox(title, source):
     client = NotionClient(token_v2)
     cv = client.get_collection_view(inbox_url)
     row = cv.collection.add_row()
@@ -28,7 +31,7 @@ def addResourcesToInbox(title, source):
     row.source = source
 
 
-def addTask(name, status):
+def add_task(name, status):
     client = NotionClient(token_v2)
     cv = client.get_collection_view(tasks_url)
     row = cv.collection.add_row()
@@ -36,51 +39,51 @@ def addTask(name, status):
     row.status = status
 
 
-def addResource(name, source, type, status, translate):
+def add_resource(name, source, type_source, status, translate):
     client = NotionClient(token_v2)
     cv = client.get_collection_view(resources_url)
     row = cv.collection.add_row()
     row.title = name
     row.source = source
-    row.type = type
+    row.type = type_source
     row.status = status
     row.translate = translate
 
 
 @app.route('/add_inbox', methods=['POST'])
 def create_inbox():
-    if isAuthorized():
+    if is_authorized():
         name = request.get_json().get('name')
         source = request.get_json().get('source')
-        addResourcesToInbox(name, source)
-        return f'success'
+        add_resources_to_inbox(name, source)
+        return f'Added to Inbox', HTTP_201_CREATED
     else:
-        return f'error: unauthorized request'
+        return f'Error. Unauthorized request', HTTP_401_UNAUTHORIZED
 
 
 @app.route('/add_task', methods=['POST'])
 def create_task():
-    if isAuthorized():
+    if is_authorized():
         name = request.get_json().get('name')
         status = request.get_json().get('status')
-        addTask(name, status)
-        return f'success'
+        add_task(name, status)
+        return f'Added to Tasks', HTTP_201_CREATED
     else:
-        return f'error: unauthorized request'
+        return f'Error. Unauthorized request', HTTP_401_UNAUTHORIZED
 
 
 @app.route('/add_resource', methods=['POST'])
 def create_resource():
-    if isAuthorized():
+    if is_authorized():
         name = request.get_json().get('name')
         source = request.get_json().get('source')
-        type = request.get_json().get('type')
+        type_source = request.get_json().get('type')
         status = request.get_json().get('status')
         translate = request.get_json().get('translate')
-        addResource(name, source, type, status, translate)
-        return f'success'
+        add_resource(name, source, type_source, status, translate)
+        return f'Added to Resources', HTTP_201_CREATED
     else:
-        return f'error: unauthorized request'
+        return f'Error. Unauthorized request', HTTP_401_UNAUTHORIZED
 
 
 if __name__ == '__main__':
